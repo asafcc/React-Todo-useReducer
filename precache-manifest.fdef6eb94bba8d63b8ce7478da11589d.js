cf91abf8aa69bc9de6e20c86ec183d7a2d8e9938
asafcc <asaf995@gmail.com>
2020-08-04 22:29:11 +0300

    Updates

---
+++ precache-manifest.fdef6eb94bba8d63b8ce7478da11589d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa301d1c402a56fe7c7c1187dd14af56",
+    "url": "/React-Todo-useReducer/index.html"
+  },
+  {
+    "revision": "2808e3b28fcd0d40e0d1",
+    "url": "/React-Todo-useReducer/static/css/main.cee506ac.chunk.css"
+  },
+  {
+    "revision": "610748a16fa3f410c181",
+    "url": "/React-Todo-useReducer/static/js/2.d535c60a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-useReducer/static/js/2.d535c60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2808e3b28fcd0d40e0d1",
+    "url": "/React-Todo-useReducer/static/js/main.a7b7d4cb.chunk.js"
+  },
+  {
+    "revision": "03d2daf8e32515fb7ea2",
+    "url": "/React-Todo-useReducer/static/js/runtime-main.6637df30.js"
+  }
+]);
